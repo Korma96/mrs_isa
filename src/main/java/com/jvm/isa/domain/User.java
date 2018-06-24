2a6369e2e3bcfc35b28d5b7b04cadc8d6e038243
Jovo <sunjkajovo@gmail.com>
2018-06-24 02:21:32 +0200

    Spreman za deploy 6

--- src/main/java/com/jvm/isa/domain/User.java
+++ src/main/java/com/jvm/isa/domain/User.java
@@ -54,6 +54,15 @@ public class User {
 		this.userType = type;
 		this.userStatus = UserStatus.ACTIVATED;
 	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 
 	public String getUsername() {
 		return username;
